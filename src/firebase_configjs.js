f9a487aa2beed4276293f29f51813aec842b4001
MayurChawla <mr.mayurchawla@gmail.com>
2021-04-18 11:52:53 +0530

    removed key

--- src/firebase_configjs.js
+++ src/firebase_configjs.js
@@ -3,7 +3,6 @@ import firebase from "firebase";
 if (firebase.apps.length === 0) {
   firebase.initializeApp(firebaseConfig);
 }
-//firebase.initializeApp(firebaseConfigkeys);
 
 const db = firebase.firestore();
 export { db };
