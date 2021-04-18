586e251dd408336deafb3206e169e7fca9a99439
MayurChawla <mr.mayurchawla@gmail.com>
2021-04-18 10:59:28 +0530

    removde keys

--- src/firebase_configjs.js
+++ src/firebase_configjs.js
@@ -1,13 +1,6 @@
 import firebase from "firebase";
 
-var firebaseConfig = {
-  apiKey: "AIzaSyAF6Kyqx15YKVh1WBviVLotXLd-iI8dfVM",
-  authDomain: "to-do-6d61c.firebaseapp.com",
-  projectId: "to-do-6d61c",
-  storageBucket: "to-do-6d61c.appspot.com",
-  messagingSenderId: "351955778246",
-  appId: "1:351955778246:web:7d1271ec33ae4467c2f5c7"
-};
+
 
 if (firebase.apps.length === 0) {
   firebase.initializeApp(firebaseConfig);
